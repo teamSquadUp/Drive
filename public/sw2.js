a808da9408bd28a1e6a32b4c02ed14d5ad383bb0
kekekekekekekekeke <kjin@hmc.edu>
2018-06-29 15:48:27 -0700

    offline page finally works : )

--- public/sw2.js
+++ public/sw2.js
@@ -1,39 +1,65 @@
-importScripts('cache-polyfill.js');
+var doCache = false;
 
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('SquadUp').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '404.html',
-        'puppy.jpg',
-        'kitten.jpg',
-        'owl.jpg',
-        'raccoon.jpg',
-        'donkey.jpg',
-        'giraffe.jpg',
-        'elephant.jpg'
-     ]);
-     console.log("cached")
-   })
- );
+// Name our cache
+var CACHE_NAME = 'my-pwa-cache-v1';
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'offline.html';
+// Delete old caches that are not our current one!
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.open(currentCache.offline)
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key);
+          }
+        }))
+      )
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-
-    console.log(event.request.url);
-    
-    event.respondWith(
-    
-    caches.match(event.request).then(function(response) {
-    
-    return response || fetch(event.request);
-    
+console.log("testing")
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+        'puppy.jpg',
+          'kitten.jpg',
+          "owl.jpg",
+          "raccoon.jpg",
+          "donkey.jpg",
+          "giraffe.jpg",
+          "elephant.jpg",
+          offlineUrl
+      ])
+      console.log("cached")
     })
-    
+  );
+});
+// The first time the user starts up the PWA, 'install' is triggered.
+
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              // Return the offline page
+              return caches.match(offlineUrl);
+          })
     );
-    
-    });
-    
-    
+  }
+  else{
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
