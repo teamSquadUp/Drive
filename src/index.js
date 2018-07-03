7cdc4833a798ecd1be27205948537d4b99898f37
kekekekekekekekeke <kjin@hmc.edu>
2018-07-03 15:24:11 -0700

    404 page routing working

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import App from './App';
 import {weatherapi} from './weatherapi'; 
 import {SwiperNoSwiping} from './SwiperNoSwiping'
 import { Router, browserHistory, Route} from 'react-router';
+import Notfound from './404'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 render((
   <Router history={browserHistory}>
     <Route exact path="/" component={App}/>
     <Route exact path="/login"/>
     <Route exact path="/weatherapi" component={weatherapi}/>
     <Route exact path="/SwiperNoSwiping" component= {SwiperNoSwiping} />
+    <Route path="*" component={Notfound} />
+
   </Router>
 ), document.getElementById('root'))
