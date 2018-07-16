c0df6c67011235c09fa6b2a7a7277b95f788bbf7
kekekekekekekekeke <kjin@hmc.edu>
2018-07-16 16:39:41 -0700

    able to uncheck

--- src/preferences.js
+++ src/preferences.js
@@ -74,151 +74,544 @@ export class Preferences extends React.Component {
 
 
     restaurantChecked(){
+
         this.setState({
             restaurants: !this.state.restaurants
         })
     }
     bakeryChecked(){
+        console.log(this.state.prefStats)
         this.setState({
             bakery: !this.state.bakery
         }) 
-    }  
+        if(this.state.prefStats.hasOwnProperty('bakery')){ 
+            var count= this.state.prefStats.bakery
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.bakery){
+            prefStats.bakery= 1+ count} 
+            else {
+                prefStats.bakery= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.bakery){
+            prefStats.bakery= 1}
+            this.setState({prefStats})
+        }
+        }
+    
     cafeChecked(){
         this.setState({
             cafe: !this.state.cafe
         })
+        if(this.state.prefStats.hasOwnProperty('cafe')){ 
+            var count= this.state.prefStats.cafe
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.cafe){
+            prefStats.cafe= 1+ count} 
+            else {
+                prefStats.cafe= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.cafe){
+            prefStats.cafe= 1}
+            this.setState({prefStats})
+        }
     }
 
     afghanChecked(){
         this.setState({
             afghani: !this.state.afghani
         })
-    }
+        if(this.state.prefStats.hasOwnProperty('afghani')){ 
+            console.log("exists")
+            var count= this.state.prefStats.afghani
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.afghani){
+            prefStats.afghani= 1+ count} 
+            else {
+                console.log("subtract 1")
+
+                prefStats.afghani= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            console.log("does not exists")
+
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.afghani){
+            prefStats.afghani= 1}
+            this.setState({prefStats})
+        }
+        }
+    
 
     africanChecked(){
         this.setState({
             african: !this.state.african
         })
+        if(this.state.prefStats.hasOwnProperty('african')){ 
+            var count= this.state.prefStats.african
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(this.state.african){
+            prefStats.african= 1+ count} 
+            else {
+                prefStats.african= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.african){
+            prefStats.african= 1}
+            this.setState({prefStats})
+        }
     }
     
     newamericanChecked(){
         this.setState({
             newamerican: !this.state.newamerican
         })
+        if(this.state.prefStats.hasOwnProperty('newamerican')){ 
+            var count= this.state.prefStats.newamerican
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(this.state.newamerican){
+            prefStats.newamerican= 1+ count} 
+            else {
+                prefStats.newamerican= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.newamerican){
+            prefStats.newamerican= 1}
+            this.setState({prefStats})
+        }
     }
 
     caribbeanChecked(){
         this.setState({
             caribbean: !this.state.caribbean
         })
+        if(this.state.prefStats.hasOwnProperty('caribbean')){ 
+            var count= this.state.prefStats.caribbean
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(this.state.caribbean){
+            prefStats.caribbean= 1+ count} 
+            else {
+                prefStats.caribbean= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.caribbean){
+            prefStats.caribbean= 1}
+            this.setState({prefStats})
+        }
     }
 
     chineseChecked(){
         this.setState({
             chinese: !this.state.chinese
         })
+        if(this.state.prefStats.hasOwnProperty('chinese')){ 
+            var count= this.state.prefStats.chinese
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.bakchineseery){
+            prefStats.chinese= 1+ count} 
+            else {
+                prefStats.chinese= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.chinese){
+            prefStats.chinese= 1}
+            this.setState({prefStats})
+        }
     }
 
     japaneseChecked(){
         this.setState({
             japanese: !this.state.japanese
         })
+        if(this.state.prefStats.hasOwnProperty('japanese')){ 
+            var count= this.state.prefStats.japanese
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.japanese){
+            prefStats.japanese= 1+ count} 
+            else {
+                prefStats.japanese= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.japanese){
+            prefStats.japanese= 1}
+            this.setState({prefStats})
+        }
     }
 
     italianChecked(){
         this.setState({
             italian: !this.state.italian
         })
+        if(this.state.prefStats.hasOwnProperty('italian')){ 
+            var count= this.state.prefStats.italian
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.italian){
+            prefStats.italian= 1+ count} 
+            else {
+                prefStats.italian= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.italian){
+            prefStats.italian= 1}
+            this.setState({prefStats})
+        }
     }
 
     indpakChecked(){
         this.setState({
             indpak: !this.state.indpak
         })
+        if(this.state.prefStats.hasOwnProperty('indpak')){ 
+            var count= this.state.prefStats.indpak
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.indpak){
+            prefStats.indpak= 1+ count} 
+            else {
+                prefStats.indpak= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.indpak){
+            prefStats.indpak= 1}
+            this.setState({prefStats})
+        }
     }
 
     koreanChecked(){
         this.setState({
             korean: !this.state.korean
         })
+        if(this.state.prefStats.hasOwnProperty('korean')){ 
+            var count= this.state.prefStats.korean
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.korean){
+            prefStats.korean= 1+ count} 
+            else {
+                prefStats.korean= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.korean){
+            prefStats.korean= 1}
+            this.setState({prefStats})
+        }
     }
 
     mexicanChecked(){
         this.setState({
             mexican: !this.state.mexican
         })
+        if(this.state.prefStats.hasOwnProperty('mexican')){ 
+            var count= this.state.prefStats.mexican
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.mexican){
+            prefStats.mexican= 1+ count} 
+            else {
+                prefStats.mexican= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.mexican){
+            prefStats.mexican= 1}
+            this.setState({prefStats})
+        }
     }
 
     asianfusionChecked(){
         this.setState({
             asianfusion: !this.state.asianfusion
         })
+        if(this.state.prefStats.hasOwnProperty('asianfusion')){ 
+            var count= this.state.prefStats.asianfusion
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.asianfusion){
+            prefStats.asianfusion= 1+ count} 
+            else {
+                prefStats.asianfusion= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.asianfusion){
+            prefStats.asianfusion= 1}
+            this.setState({prefStats})
+        }
     }
 
     pizzaChecked(){
         this.setState({
             pizza: !this.state.pizza
         })
+        if(this.state.prefStats.hasOwnProperty('pizza')){ 
+            var count= this.state.prefStats.pizza
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.pizza){
+            prefStats.pizza= 1+ count} 
+            else {
+                prefStats.pizza= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.pizza){
+            prefStats.pizza= 1}
+            this.setState({prefStats})
+        }
     }
 
     bbqChecked(){
         this.setState({
             bbq: !this.state.bbq
         })
+        if(this.state.prefStats.hasOwnProperty('bbq')){ 
+            var count= this.state.prefStats.bbq
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.bbq){
+            prefStats.bbq= 1+ count} 
+            else {
+                prefStats.bbq= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.bbq){
+            prefStats.bbq= 1}
+            this.setState({prefStats})
+        }
     }
 
     vegetarianChecked(){
         this.setState({
             vegetarian: !this.state.vegetarian
         })
+        if(this.state.prefStats.hasOwnProperty('vegetarian')){ 
+            var count= this.state.prefStats.vegetarian
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.vegetarian){
+            prefStats.vegetarian= 1+ count} 
+            else {
+                prefStats.vegetarian= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.vegetarian){
+            prefStats.vegetarian= 1}
+            this.setState({prefStats})
+        }
     }
 
     glutenfreeChecked(){
         this.setState({
             gluten_free: !this.state.gluten_free
         })
+        if(this.state.prefStats.hasOwnProperty('gluten_free')){ 
+            var count= this.state.prefStats.gluten_free
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.gluten_free){
+            prefStats.gluten_free= 1+ count} 
+            else {
+                prefStats.gluten_free= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.gluten_free){
+            prefStats.gluten_free= 1}
+            this.setState({prefStats})
+        }
     }
 
     delisChecked(){
         this.setState({
             delis: !this.state.delis
         })
+        if(this.state.prefStats.hasOwnProperty('delis')){ 
+            var count= this.state.prefStats.delis
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.delis){
+            prefStats.delis= 1+ count} 
+            else {
+                prefStats.delis= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.delis){
+            prefStats.delis= 1}
+            this.setState({prefStats})
+        }
     }
 
     dinersChecked(){
         this.setState({
             diners: !this.state.diners
         })
+        if(this.state.prefStats.hasOwnProperty('diners')){ 
+            var count= this.state.prefStats.diners
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.diners){
+            prefStats.diners= 1+ count} 
+            else {
+                prefStats.v= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.diners){
+            prefStats.diners= 1}
+            this.setState({prefStats})
+        }
     }
 
     burgersChecked(){
         this.setState({
             burgers: !this.state.burgers
         })
+        if(this.state.prefStats.hasOwnProperty('burgers')){ 
+            var count= this.state.prefStats.burgers
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.burgers){
+            prefStats.burgers= 1+ count} 
+            else {
+                prefStats.burgers= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.burgers){
+            prefStats.burgers= 1}
+            this.setState({prefStats})
+        }
     }
 
     saladChecked(){
         this.setState({
             salad: !this.state.salad
         })
+        if(this.state.prefStats.hasOwnProperty('salad')){ 
+            var count= this.state.prefStats.salad
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.salad){
+            prefStats.salad= 1+ count} 
+            else {
+                prefStats.salad= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.salad){
+            prefStats.salad= 1}
+            this.setState({prefStats})
+        }
     }
 
     wrapsChecked(){
         this.setState({
             wraps: !this.state.wraps
         })
+        if(this.state.prefStats.hasOwnProperty('wraps')){ 
+            var count= this.state.prefStats.wraps
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.wraps){
+            prefStats.wraps= 1+ count} 
+            else {
+                prefStats.wraps= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.wraps){
+            prefStats.wraps= 1}
+            this.setState({prefStats})
+        }
     }
 
     noodlesChecked(){
         this.setState({
             noodles: !this.state.noodles
         })
+        if(this.state.prefStats.hasOwnProperty('noodles')){ 
+            var count= this.state.prefStats.noodles
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.noodles){
+            prefStats.noodles= 1+ count} 
+            else {
+                prefStats.noodles= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.noodles){
+            prefStats.noodles= 1}
+            this.setState({prefStats})
+        }
     }
 
     hotpotChecked(){
         this.setState({
             hotpot: !this.state.hotpot
         })
+        if(this.state.prefStats.hasOwnProperty('hotpot')){ 
+            var count= this.state.prefStats.hotpot
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.hotpot){
+            prefStats.hotpot= 1+ count} 
+            else {
+                prefStats.hotpot= count-1 
+            }
+            this.setState({prefStats})
+        }
+        else{   
+            let prefStats= Object.assign({}, this.state.prefStats)
+            if(!this.state.hotpot){
+            prefStats.hotpot= 1}
+            this.setState({prefStats})
+        }
     }
     componentDidMount(){
         var currentComponent = this
